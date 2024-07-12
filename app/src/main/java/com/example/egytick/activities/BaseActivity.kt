419f83c2b6cbfdbae13e3870b443704ab72fe37b
Petmadsh <peteremadsh9@gmail.com>
2024-07-13 00:15:13 +0200

    BottomNavbar

--- app/src/main/java/com/example/egytick/activities/BaseActivity.kt
+++ app/src/main/java/com/example/egytick/activities/BaseActivity.kt
@@ -31,13 +31,5 @@ open class BaseActivity : AppCompatActivity() {
         snackBarView.setBackgroundColor(color)
         snackBar.show()
     }
-    private fun replaceFragment(fragment : Fragment){
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
-        fragmentTransaction.commit()
-
-
-    }
 }
